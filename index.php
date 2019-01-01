b8e7c36684ff456681f66b303d89fec878c5e8ab
Christophe <cavo789@yahoo.com>
2019-01-01 17:29:16 +0100

    Abandonment of jQuery and migration to vue.js

--- index.php
+++ index.php
@@ -18,6 +18,9 @@ declare(strict_types = 1);
  * - Call marked.js (https://www.npmjs.com/package/marked) to render the table from markdown
  *         to html
  * - Add bootstrap class to the rendered table
+ *
+ * Last mod:
+ * 2019-01-01 - Abandonment of jQuery and migration to vue.js
  */
 
 define('REPO', 'https://github.com/cavo789/marknotes_xls2md');
@@ -61,14 +64,95 @@ if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
         <?php echo $github; ?>
         <div class="container">
             <div class="page-header"><h1>Marknotes - XLS2MD</h1></div>
-            <div class="container">
+            <div class="container" id="app">
                 <div class="form-group">
                     <label for="editor">Simply copy, from Excel, a table and paste here to convert 
                         table to markdown.</label>
-                    <details>
+                    <how-to-use demo="https://raw.githubusercontent.com/cavo789/marknotes_xls2md/master/image/demo.gif"></how-to-use>
+                    <textarea class="form-control" rows="5" v-model="editor" name="editor" @change="onChange" @paste="onPaste"></textarea>
+                </div>
+                <hr/>
+                <div v-if="Markdown!==''">
+                    <h2 id="markdown">Markdown code <small style="font-size:0.4em"><a href="#html">See HTML rendering</a></small></h2>
+                    <pre v-html="Markdown"></pre>
+                    <hr/>
+                </div>
+                <div v-if="HTML!==''">
+                    <h2 id="html">HTML rendering <small style="font-size:0.4em"><a href="#markdown">See Markdown code</a></small></h2>
+                    <pre v-html="HTML"></pre>
+                    <hr/>
+                </div>
+            </div>
+        </div>
+        <script src="https://unpkg.com/vue"></script>
+        <script src="https://unpkg.com/marked@0.3.6"></script>
+        <script type="text/javascript">
+
+            function columnWidth(rows, columnIndex) {
+                return Math.max.apply(
+                null,
+                rows.map(function(row) {
+                    return row[columnIndex].length;
+                })
+                );
+            }
+
+            function getMarkdown(data) {
+                var rows = data.split(/[\n\u0085\u2028\u2029]|\r\n?/g).map(function(row) {
+                    return row.split("\t");
+                });
+
+                var columnWidths = rows[0].map(function(column, columnIndex) {
+                    return columnWidth(rows, columnIndex);
+                });
+
+                var markdownRows = rows.map(function(row, rowIndex) {
+                    // | Name         | Title | Email Address  |
+                    // |--------------|-------|----------------|
+                    // | Jane Atler   | CEO   | jane@acme.com  |
+                    // | John Doherty | CTO   | john@acme.com  |
+                    // | Sally Smith  | CFO   | sally@acme.com |
+                    return (
+                        "| " +
+                        row
+                        .map(function(column, index) {
+                            return (
+                            column + Array(columnWidths[index] - column.length).join(" ")
+                            );
+                        })
+                        .join(" | ") +
+                        " |"
+                    );
+                    row.map;
+                });
+
+                markdownRows.splice(
+                    1,
+                    0,
+                    "| " +
+                        columnWidths
+                        .map(function(width, index) {
+                            return Array(columnWidths[index]).join("-");
+                        })
+                        .join(" | ") +
+                        " |"
+                );
+
+                // Get the markdown representation of the pasted info
+                return markdownRows.join("\n");
+            }
+
+            Vue.component('how-to-use', {
+                props: {
+                    demo: {
+                        type: String,
+                        required: true
+                    }
+                },
+                template:
+                    `<details>
                         <summary>How to use?</summary>
-                        <div class="container-fluid">
-                            <div class="row">
+                        <div class="row">
                                 <div class="col-sm">
                                     <ol>
                                         <li>In your Excel worksheet, select a range like f.i. 
@@ -82,21 +166,42 @@ if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
                                     <p>Adjust the markdown syntax if needed and copy/paste into your 
                                         notes.</p>
                                 </div>
-                                <div class="col-sm">
-                                    <img height="300px" src="https://raw.githubusercontent.com/cavo789/marknotes_xls2md/master/image/demo.gif" alt="Demo">
-                                </div>
+                                <div class="col-sm"><img v-bind:src="demo" alt="Demo"></div>
                             </div>
                         </div>
-                    </details>
-                    <textarea class="form-control" rows="10" id="editor" name="editor"></textarea>
-                </div>
-                <hr/>
-                <pre id="Markdown">&nbsp;</pre>
-            </div>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous" type="text/javascript"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="https://unpkg.com/marked@0.3.6"></script>
-        <script src="script.js" type="text/javascript"></script>
+                    </details>`
+            });
+
+            var app = new Vue({
+                el: '#app',
+                data: {
+                    editor: '',
+                    Markdown: ''
+                },
+                methods: {
+                    onChange(event) {
+                        this.Markdown = getMarkdown(this.editor);
+                    },
+                    onPaste(event) {
+                        event.preventDefault();
+                        var clipboard = event.clipboardData;
+                        this.editor = clipboard.getData("text/plain").trim();
+                        this.Markdown = getMarkdown(this.editor);
+                    }
+                },
+                computed: {
+                    HTML() {
+                        if (this.Markdown == '') {
+                            return '';
+                        }
+                        // Call marked() to convert the MD string into a HTML table
+                        var mdTable = marked(this.Markdown, { sanitize: true });
+                        // Add Boostrap classes
+                        mdTable = mdTable.replace('<table>', '<table class="table table-hover table-striped">');
+                        return mdTable;
+                    }
+                }
+            });
+        </script>
     </body>
 </html>
